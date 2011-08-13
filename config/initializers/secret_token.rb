cd9f4a7b9d5c24e1a5bbb4a1d4fde185978b6a1d
Linda Keating <linda@secondage.com>
2011-08-13 19:58:05 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrowdshareApp::Application.config.secret_token = 'cc9bc8560b852fe4e324fd816a889e0df604d811f420f88105a48508e1362d4836403d7c429a5569ce1785fb0a0f021d0cf885e7f09777a3211dad6d9663f3ed'
