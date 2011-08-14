3751ae469bd819f43870cd5416224aae39b01558
Linda Keating <linda@secondage.com>
2011-08-14 16:37:02 +0100

    added a title helper and stylesheets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Return a title on a per - page basis
   def title
-    base_title ="Crowdshare Application"
+    base_title ="Crowdshare | Online Sharing"
     if @title.nil?
       base_title
     else
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each)do
+    @base_title = "Crowdshare | Online Sharing"
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -12,7 +16,7 @@ describe PagesController do
     it "should have the right title" do
       get "home"
       response.should have_selector("title",
-                        :content =>"Crowdshare | Home")
+                                    :content => "#{@base_title} | Home")
     end
     it "should have a non black body" do
       get "home"
@@ -29,7 +33,7 @@ describe PagesController do
      it "should have the right title" do
       get "contact"
       response.should have_selector("title",
-                        :content =>"Crowdshare | Contact")
+                                    :content =>"#{@base_title} | Contact")
     end
   end
 
@@ -42,7 +46,7 @@ describe PagesController do
      it "should have the right title" do
       get "about"
       response.should have_selector("title",
-                        :content =>"Crowdshare | About")
+                                    :content =>"#{@base_title} | About")
     end
   end
 
